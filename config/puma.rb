04e18e3a07d5ecccc5eb048ecb164e694d39e22b
Yann Klein <yann.klein@me.com>
2026-06-02 10:10:08 +0200

    setup solid queue

--- config/puma.rb
+++ config/puma.rb
@@ -40,3 +40,7 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+if ENV['RAILS_ENV'] == 'development'
+  plugin :solid_queue
+end
